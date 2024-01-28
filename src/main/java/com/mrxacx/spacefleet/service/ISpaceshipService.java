16ba4b3c9f9179668ff5233f3235cf3d114f7632
MrXacx <misterxacx@gmail.com>
2024-01-27 22:06:35 -0300

    feat: create spaceship service interface

--- src/main/java/com/mrxacx/spacefleet/service/ISpaceshipService.java
+++ src/main/java/com/mrxacx/spacefleet/service/ISpaceshipService.java
@@ -1,2 +1,23 @@
-package com.mrxacx.spacefleet.service;public interface ISpaceshipService {
+package com.mrxacx.spacefleet.service;
+
+import com.mrxacx.spacefleet.resource.dto.RepairDTO;
+import com.mrxacx.spacefleet.model.Maintenance;
+import com.mrxacx.spacefleet.model.Repair;
+import com.mrxacx.spacefleet.model.Spaceship;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ISpaceshipService {
+    Spaceship recordSpaceship(String model);
+    Spaceship fetchSpaceship(String spaceshipId);
+    List<Spaceship> fetchSpaceshipsForName(String name);
+    List<Spaceship> fetchSpaceshipsForModel(String model);
+    List<Spaceship> fetchSpaceshipsForManufacter(String manufacter);
+    Repair recordSpaceshipRepair(RepairDTO repairDTO);
+    Repair fetchSpaceshipRepair(String repairId);
+    Repair finishSpaceshipRepair(String repairId);
+    Maintenance recordSpaceshipMaintenance(String spaceshipId);
+
+
 }
