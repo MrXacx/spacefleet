db6104da70af89a4e4c57c8642eb36122128b1bc
MrXacx <misterxacx@gmail.com>
2024-01-28 18:51:37 -0300

    feat: create custom finder methods

--- src/main/java/com/mrxacx/spacefleet/repository/ISpaceshipRepository.java
+++ src/main/java/com/mrxacx/spacefleet/repository/ISpaceshipRepository.java
@@ -2,6 +2,12 @@ package com.mrxacx.spacefleet.repository;
 
 import com.mrxacx.spacefleet.model.Spaceship;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.UUID;
 
-public interface ISpaceshipRepository extends JpaRepository<Spaceship, UUID> {}
+public interface ISpaceshipRepository extends JpaRepository<Spaceship, UUID> {
+    List<Spaceship> findByName(String name);
+    List<Spaceship> findByModel(String model);
+    List<Spaceship> findByManufacter(String manufacter);
+}
--- src/main/java/com/mrxacx/spacefleet/service/impl/SpaceshipService.java
+++ src/main/java/com/mrxacx/spacefleet/service/impl/SpaceshipService.java
@@ -1,4 +1,94 @@
-package com.mrxacx.spacefleet.service;
+package com.mrxacx.spacefleet.service.impl;
 
-public class SpaceshipService {
+import com.mrxacx.spacefleet.resource.dto.RepairDTO;
+import com.mrxacx.spacefleet.model.Maintenance;
+import com.mrxacx.spacefleet.model.Repair;
+import com.mrxacx.spacefleet.model.Spaceship;
+import com.mrxacx.spacefleet.repository.IMaintenanceRepository;
+import com.mrxacx.spacefleet.repository.IRepairRepository;
+import com.mrxacx.spacefleet.repository.ISpaceshipRepository;
+import com.mrxacx.spacefleet.service.ISpaceshipService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class SpaceshipService implements ISpaceshipService {
+
+    private ISpaceshipRepository spaceshipRepository;
+    private IMaintenanceRepository maintenanceRepository;
+    private IRepairRepository repairRepository;
+
+    @Override
+    public Spaceship recordSpaceship(String model) {
+        return null;
+    }
+
+    @Override
+    public Spaceship fetchSpaceship(String spaceshipId) {
+        return spaceshipRepository
+                .findById(UUID.fromString(spaceshipId))
+                .orElseThrow();
+    }
+
+    @Override
+    public List<Spaceship> fetchSpaceshipsForName(String name) {
+        return spaceshipRepository
+                .findAll((Spaceship sp) -> sp.getName().equals(name));
+    }
+
+    @Override
+    public List<Spaceship> fetchSpaceshipsForModel(String model) {
+        return spaceshipRepository
+                .findAll((Spaceship sp) -> sp.getModel().equals(model));
+    }
+
+    @Override
+    public List<Spaceship> fetchSpaceshipsForManufacter(String manufacter) {
+        return spaceshipRepository
+                .findAll((Spaceship sp) -> sp.getManufacter().equals(manufacter));
+    }
+
+    @Override
+    public Repair recordSpaceshipRepair(RepairDTO repairDTO) {
+        final Spaceship spaceship = retrieveSpaceship(repairDTO.getSpaceshipId());
+
+        return repairRepository
+                .save(
+                        Repair.builder()
+                                .spaceship(spaceship)
+                                .fault(repairDTO.getFault())
+                                .build()
+                );
+    }
+
+    @Override
+    public Repair fetchSpaceshipRepair(String repairId) {
+        return repairRepository
+                .findById(UUID.fromString(repairId))
+                .orElseThrow();
+    }
+
+    @Override
+    public Repair finishSpaceshipRepair(String repairId) {
+        Repair repair = retrieveSpaceshipRepair(repairId);
+        repair.setFinished(true);
+        return repairRepository.save(repair);
+    }
+
+    @Override
+    public Maintenance recordSpaceshipMaintenance(String spaceshipId) {
+        final Spaceship spaceship = retrieveSpaceship(spaceshipId);
+
+        return maintenanceRepository
+                .save(
+                        Maintenance
+                                .builder()
+                                .spaceship(spaceship)
+                                .build()
+                );
+    }
 }
