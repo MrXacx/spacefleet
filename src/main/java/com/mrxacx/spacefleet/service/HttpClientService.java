9143d07adf24769497767964a3252c8be9a72560
MrXacx <misterxacx@gmail.com>
2024-01-30 12:02:00 -0300

    feat: create HTTP Client abstraction

--- src/main/java/com/mrxacx/spacefleet/service/HttpClientService.java
+++ src/main/java/com/mrxacx/spacefleet/service/HttpClientService.java
@@ -1,2 +1,30 @@
-package com.mrxacx.spacefleet.service;public class c {
+package com.mrxacx.spacefleet.service;
+
+import com.mrxacx.spacefleet.exception.UnexpectedHttpResponseException;
+import com.mrxacx.spacefleet.model.IModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * @author ariel
+ * @since 1.0
+ */
+@RequiredArgsConstructor
+@Service
+public abstract class HttpClientService {
+  final protected RestTemplate api;
+  final protected String uri;
+  
+  protected abstract IModel getModel(String path, Class<? extends  IModel> modelClass);
+  protected abstract List<? extends IModel> getModelList(String path, Class<? extends  IModel> modelClass);
+  
+  protected void catchUnsucessfulRequest(ResponseEntity<?> response){
+    if (!response.getStatusCode().is2xxSuccessful()){ // If the HTTP status code be different of 2XX
+      throw new UnexpectedHttpResponseException("Falha na pesquisa: "+ response.toString());
+    }
+  }
 }
