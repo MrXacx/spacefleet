ae3f38f22b480a965e235e14b60b67768f07bd18
MrXacx <misterxacx@gmail.com>
2024-01-30 20:25:14 -0300

    feat: config routes to basic read and insert spaceships

--- src/main/java/com/mrxacx/spacefleet/controller/SpaceshipController.java
+++ src/main/java/com/mrxacx/spacefleet/controller/SpaceshipController.java
@@ -1,20 +1,40 @@
-package com.mrxacx.spacefleet.resource.dto;
+package com.mrxacx.spacefleet.controller;
 
+import com.mrxacx.spacefleet.controller.dto.SpaceshipDTO;
 import com.mrxacx.spacefleet.model.Spaceship;
 import com.mrxacx.spacefleet.service.impl.SpaceshipService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequestMapping("/spaceship")
-@RestController()
 @RequiredArgsConstructor
+@RestController
 public class SpaceshipController {
   final SpaceshipService spaceshipService;
   
   @GetMapping("/{id}")
-  public Spaceship fetchSpaceship(@String id){
+  public Spaceship fetchSpaceship(@PathVariable("id") String id) {
+    try {
+      return spaceshipService
+          .fetchSpaceship(
+              UUID.fromString(id)
+          );
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return null;
+    }
+  }
   
+  @PostMapping
+  public Spaceship recordSpaceship(@RequestBody SpaceshipDTO spaceshipDTO) {
+    try {
+      return spaceshipService
+        .recordSpaceship(spaceshipDTO.getModel());
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return null;
+    }
   }
 }
