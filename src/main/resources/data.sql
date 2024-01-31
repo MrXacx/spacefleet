41900b46ed7f0e2f6e0f367363552bf37b048e4d
MrXacx <misterxacx@gmail.com>
2024-01-31 17:09:25 -0300

    test: create mocked spaceships SQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO SPACESHIP (id, name, model, manufacturer) VALUES
+(UUID(), 'Nave 1', 'Modelo 1', 'Fabricante 1'),
+(UUID(), 'Nave 2', 'Modelo 2', 'Fabricante 2'),
+(UUID(), 'Nave 3', 'Modelo 3', 'Fabricante 3'),
+(UUID(), 'Nave 4', 'Modelo 4', 'Fabricante 4');
