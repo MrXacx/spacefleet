2d4a9a88c0ad0be891fa40ac4172fd2760d6557a
MrXacx <misterxacx@gmail.com>
2024-02-02 17:58:54 -0300

    fix: configure default values of Journey attributes Signed-off-by: MrXacx <misterxacx@gmail.com>
    Signed-off-by: MrXacx <misterxacx@gmail.com>

--- src/main/java/com/mrxacx/spacefleet/model/Journey.java
+++ src/main/java/com/mrxacx/spacefleet/model/Journey.java
@@ -8,6 +8,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -25,9 +26,11 @@ public class Journey implements IModel {
   @ManyToOne(optional = false, fetch = FetchType.LAZY)
   @JsonIgnore
   private Spaceship spaceship;
-  private Date date;
+  @Builder.Default
+  private Date date = new Date();
   private Date duration;
   @ManyToMany
-  private List<CrewMember> crew;
+  @Builder.Default
+  private List<CrewMember> crew = new ArrayList<>();
   private String goal;
 }
